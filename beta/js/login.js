16d1d26c63598ac7ec16602dd51629b1ffbabe84
julenalonsodev <julen.alonso.dev@gmail.com>
2025-12-01 16:31:36 +0100

    Carpetas movidas

--- beta/js/login.js
+++
@@ -1,5 +0,0 @@
-        // Simulación de login (sin conexión a base de datos)
-        document.getElementById('loginForm').addEventListener('submit', function (e) {
-            e.preventDefault();
-            window.location.href = 'home.html'; // Redirección ficticia
-        });
